520dfe8bec24855a06d7ca9b70e04b5a80f24d0a
LVillaflores <lorenzo@tanda.co>
2019-10-25 09:06:18 +0800

    Updated c variable

--- Example.py
+++ Example.py
@@ -3,7 +3,7 @@ import numpy as np
 a = np.arange(15).reshape(3, 5)  # Create array using numpy with 3 rows and 5 columns
 b = np.arange(15) ** 2  # Array with range 15 raised to 2
 b = b.reshape(3, 5)  # Reshape array to have 3 rows and 5 columns
-c = np.arange(20).reshape(2, 5)
+c = np.arange(20)
 
 print('Array a')
 
@@ -16,4 +16,5 @@ print('Values of array b:')
 for element in b.flat:  # Iterate over all elements of the array
 	print(element)  # Divides each element by 3 then prints out
 
-# Added Version Control
+for e in c:
+	print(e)
